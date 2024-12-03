2386e50c37a7f62e9a4a09e0de4abd4bc914c543
MamenHD <147691861+MamenHD@users.noreply.github.com>
2024-12-03 21:20:34 +0700

    Create main9.js

---
+++ main9.js
@@ -0,0 +1,16 @@
+function multiplier(x) {
+    return function (num) {
+      return x * num;
+    };
+  }
+  
+  const double = multiplier(2);
+  const triple = multiplier(3);
+  
+  console.log(double(10));
+  console.log(triple(11));
+  
+  /* Output:
+  20
+  33
+  */
