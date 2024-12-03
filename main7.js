38fb87e2a1b027422fa97c8888d110da8ea1fd59
MamenHD <147691861+MamenHD@users.noreply.github.com>
2024-12-03 21:19:42 +0700

    Create main7.js

---
+++ main7.js
@@ -0,0 +1,11 @@
+function convertCelsiusToFahrenheit(temperature) {
+    const result = (9 / 5) * temperature + 32;
+    return result;
+  }
+  
+  const temperatureInFahrenheit = convertCelsiusToFahrenheit(90);
+  console.log('Hasil konversi:', temperatureInFahrenheit);
+  
+  /* Output:
+  Hasil konversi: 194
+  */
